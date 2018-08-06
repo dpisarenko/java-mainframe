f4bd2abc3e6721d77e289636999d1e221fd19d2d
dp118m <dp@altruix.co>
2018-08-06 18:56:40 +0300

    Binary gap example.

---
+++ 01/COBOL/Program1.cbl
@@ -0,0 +1,13 @@
+       program-id. Program1 as "Cobol01.Program1".
+
+       data division.
+       working-storage section.
+
+       procedure division.
+       
+           display 'Hello, Cobol'.
+           stop ' '.
+           stop run.
+           goback.
+           
+       end program Program1.
