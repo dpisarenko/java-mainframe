8b9827bc72a244146a80573533505c945e1b6d21
dp118m <dp@altruix.co>
2018-08-08 14:28:30 +0300

    Binary gap example: Writing the COBOL program.

--- 01-BinaryGap/COBOL/Program1.cbl
+++ 01-BinaryGap/COBOL/Program1.cbl
@@ -1,13 +1,20 @@
-       program-id. Program1 as "Cobol01.Program1".
+       identification division.
+       program-id. Program1.
+
+       environment division.
+       configuration section.
 
        data division.
        working-storage section.
-
+       01 N PIC 9(5).
+      *PIC 9(5).
+      *N PIC 9(5).
+      *Highest number is 99999.
+     
        procedure division.
-       
-           display 'Hello, Cobol'.
-           stop ' '.
-           stop run.
+      * Next step: Find out how to define a numeric variable.
+           display 'Hello world'.
+           STOP run.
            goback.
-           
+
        end program Program1.
