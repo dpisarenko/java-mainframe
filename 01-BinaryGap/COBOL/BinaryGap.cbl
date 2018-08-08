022b6741be5a8a8db6bc9ad7005b4827a67497ea
dp118m <dp@altruix.co>
2018-08-08 14:44:27 +0300

    Binary gap, COBOL.

--- 01-BinaryGap/COBOL/BinaryGap.cbl
+++ 01-BinaryGap/COBOL/BinaryGap.cbl
@@ -10,10 +10,17 @@
       *find out the maximal binary gap in the binary representation of 
       *N.
        01 N PIC 9(5) VALUE 10.
-     
+      *Length of the binary representation is calculated according
+      *to formula ln(n) / ln(2).
+      *Source: https://stackoverflow.com/questions/7150035/calculating-
+      *bits-required-to-store-decimal-number
+      *Let's say the maximum value N can take is 2147483647.
+      *CEIL(ln(2147483647)/ln(2)) = CEIL(21.4876 / 0.6931) =
+      *= CEIL (31.0022) = 32
+       01 BIN-REP PIC 9(32).
        procedure division.
       * Next step: How many bits does binary representation of 99999 
-        contain?
+      * contain?
            display 'Hello world'.
            display N.
            stop 'Press Enter to continue'.
