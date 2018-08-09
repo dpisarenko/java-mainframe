f7b6bfdd5268e04c375bef3faa8fd2a3a180bd3e
dp118m <dp@altruix.co>
2018-08-09 16:00:14 +0300

    FrogJmp: Implemented a simple version of CEIL function in COBOL.

--- 02-FrogJmp/COBOL/FrogJmp.cbl
+++ 02-FrogJmp/COBOL/FrogJmp.cbl
@@ -11,13 +11,19 @@
        01 Y pic 9(10) value 85.
        01 D pic 9(10) value 30.
        01 DIST pic 9(10)V99 value 0.
+      *DEC-PART will contain the decimal part (two decimal digits after
+      *V) of DIST after division.
+       01 DEC-PART pic 99 value 0.
       *RES will contain the result. 
-       01 RES pic 9(10) value 30.
+       01 RES pic 9(10) value 0.
        procedure division.
            display 'FrogJmp'.
            compute DIST = Y - X.
            divide DIST by D giving DIST.
-           compute RES = function ceil(DIST).
+           move DIST(11:2) to DEC-PART.
+           move DIST(1:10) to RES.
+           if DEC-PART > 0 then
+               add 1 to RES.
            display 'Result:'.
            display RES.
            stop ' '.
