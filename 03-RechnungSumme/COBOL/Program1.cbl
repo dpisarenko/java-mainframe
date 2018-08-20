253b2ea1366842459eea1e83ca397bbf1685948e
dp118m <dp@altruix.co>
2018-08-20 19:02:27 +0300

    RechungSumme: Working on the COBOL part.

--- 03-RechnungSumme/COBOL/Program1.cbl
+++ 03-RechnungSumme/COBOL/Program1.cbl
@@ -1,5 +1,5 @@
        identification division.
-       program-id. Program1.
+       program-id. RechnungSumme.
 
        environment division.
        input-output section.
@@ -9,10 +9,28 @@
        configuration section.
 
        data division.
+       file section.
+      *Input file
+       fd infile.
+       01 inrec.
+         03 in-name         pic X(20).
+         03 in-quant        pic 9(2).
+         03 in-unit-price   pic 9(3)V99.
+         03 in-rebate       pic 9.
+         03 in-vat          pic 9.
+      *Output file.
+       fd outfile.
+       01 outrec.
+         03 out-rec-type    pic 9.
+         03 out-name        pic X(20).
+         03 out-quant       pic 9(2).
+         03 out-price-novat pic 9(3)V99.
+         03 out-price-vat   pic 9(3)V99.
        working-storage section.
 
        procedure division.
-
+           stop ' '.
+           stop run.
            goback.
 
-       end program Program1.
+       end program RechnungSumme.
