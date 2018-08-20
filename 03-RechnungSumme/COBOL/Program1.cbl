f00849bce1d94ce115e70a32445a861a1bc28494
dp118m <dp@altruix.co>
2018-08-20 19:38:32 +0300

    RechungSumme: Working on the COBOL part.

--- 03-RechnungSumme/COBOL/Program1.cbl
+++ 03-RechnungSumme/COBOL/Program1.cbl
@@ -4,11 +4,11 @@
        environment division.
        input-output section.
        file-control.
-           select infile assign inname.
-           select outfile assign outname.
-       configuration section.
+           select infile assign INNAME
+               ORGANIZATION SEQUENTIAL.
+           select outfile assign OUTNAME
+               ORGANIZATION SEQUENTIAL.
 
-       data division.
        file section.
       *Input file
        fd infile.
@@ -27,8 +27,30 @@
          03 out-price-novat pic 9(3)V99.
          03 out-price-vat   pic 9(3)V99.
        working-storage section.
-
+       01 eof-flag pic 9 value 0.
        procedure division.
+       main.
+           perform init-para.
+      *    perform BOD-PARA.
+           perform end-para.
+       init-para.
+           open input infile.
+           open output outfile.
+           PERFORM READ-INPUT-FILE.
+       BOD-PARA.
+           PERFORM PROCESS-REC
+               UNTIL eof-flag = 1.
+       READ-INPUT-FILE.
+           read infile
+               at end
+                   move 1 to eof-flag.
+       PROCESS-REC.
+           display 'in-name:'.
+           display in-name.
+           PERFORM READ-INPUT-FILE.
+       end-para.
+           close infile outfile.
+           display 'Press <CR> to exit'.
            stop ' '.
            stop run.
            goback.
