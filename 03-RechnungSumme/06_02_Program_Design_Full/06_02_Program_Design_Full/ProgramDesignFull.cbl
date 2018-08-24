54465ca474fdb630b8d3162efd2084230e3a6775
dp118m <dp@altruix.co>
2018-08-24 17:15:58 +0300

    RechnungSumme: Implementing input of data.

--- 03-RechnungSumme/06_02_Program_Design_Full/06_02_Program_Design_Full/ProgramDesignFull.cbl
+++ 03-RechnungSumme/06_02_Program_Design_Full/06_02_Program_Design_Full/ProgramDesignFull.cbl
@@ -8,25 +8,15 @@
        FILE SECTION.
        FD INFILE.
        01  RECORD-TYPE-A.
-           05  SR-NAME                 PIC X(19).
-           05                          PIC X(5).
-           05  SR-ADDRESS              PIC X(20).
-           05                          PIC XXXXX.
-           05  SR-PHONE                PIC X(7).
-           05                          PIC XXX.
-           05  SR-BIRTH-DATE           PIC X(6).
-           05                          PIC XXXX.
-           05  SR-RECORD-TYPE          PIC X.
-           05                          PIC X(4).
-       01  RECORD-TYPE-B.
-           05  CR-NAME                 PIC X(19).
-           05                          PIC X(5).
-           05  CR-COURSE-NUMBER        PIC X(5).
-           05                          PIC X(5).
-           05  CR-CREDITS              PIC 9.
-           05                          PIC X(34).
-           05  CR-RECORD-TYPE          PIC X.
-           05                          PIC X(4).
+           05 in-name         pic X(20).
+           05                 pic x.
+           05 in-quant        pic 9(2).
+           05                 pic x.
+           05 in-unit-price   pic 9(3)V99.
+           05                 pic x.
+           05 in-rebate       pic 9.
+           05                 pic x.
+           05 in-vat          pic 9.
        FD OUTFILE.
        01  OUTREC.
            05  OUT-NAME                PIC X(19).
@@ -55,18 +45,9 @@
             STOP 'PRESS <CR> TO STOP'
             STOP RUN.
        PROCESS-REC.
-            IF SR-RECORD-TYPE = 'A'
-                PERFORM TYPE-A-ACTIONS
-            ELSE 
-                PERFORM TYPE-B-ACTIONS
-            END-IF
-            PERFORM READ-INPUT-FILE.
-       TYPE-A-ACTIONS.
-            MOVE SR-NAME TO OUT-NAME
+            MOVE IN-NAME TO OUT-NAME
             WRITE OUTREC
             DISPLAY OUTREC.
-        TYPE-B-ACTIONS .
-            ADD 1 TO WS-TYPE-B-COUNTER.
        READ-INPUT-FILE.
             READ INFILE
                AT END MOVE 1 TO WS-END-OF-FILE
